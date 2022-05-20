787d3f2dd0211a2e8baf5ebb5ceb2507fec2b479
yusufnoor786 <yusufnoor786@gamil.com>
2022-05-20 10:37:13 +0530

    Revert "Build GarageHub into Settings"  we Dont need This Anymore
    This reverts commit 453da8b9e2b03b6eb7c2133eb96037fd34aab20d.
    
    Sign-off-By: Yusufnoor786 <yusufnoor786@gmail.com>

--- src/com/android/settings/core/gateway/SettingsGateway.java
+++ src/com/android/settings/core/gateway/SettingsGateway.java
@@ -168,8 +168,6 @@ import com.android.settings.wifi.p2p.WifiP2pSettings;
 import com.android.settings.wifi.savedaccesspoints2.SavedAccessPointsWifiSettings2;
 import com.android.settings.wifi.tether.WifiTetherSettings;
 
-import com.garage.settings.garage;
-
 public class SettingsGateway {
 
     /**
@@ -325,8 +323,7 @@ public class SettingsGateway {
             NetworkProviderSettings.class.getName(),
             NetworkSelectSettings.class.getName(),
             AlarmsAndRemindersDetails.class.getName(),
-            MediaManagementAppsDetails.class.getName(),
-	    garage.class.getName()
+            MediaManagementAppsDetails.class.getName()
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
@@ -367,6 +364,5 @@ public class SettingsGateway {
             Settings.MyDeviceInfoActivity.class.getName(),
             Settings.ModuleLicensesActivity.class.getName(),
             UserBackupSettingsActivity.class.getName(),
-	    Settings.garageActivity.class.getName(),
     };
 }
