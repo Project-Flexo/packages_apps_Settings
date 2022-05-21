4eabc8c9279345a408c78480ee4fd029f364d26d
yusufnoor786 <zuzu950444@gamil.com>
2022-05-22 02:58:11 +0530

    Revert "Settings/Altruist: Initial checkin of Altruist"
    This reverts commit 4ab62d804b7c8220dcedca9234b7f179468baace.

--- src/com/android/settings/core/gateway/SettingsGateway.java
+++ src/com/android/settings/core/gateway/SettingsGateway.java
@@ -168,8 +168,6 @@ import com.android.settings.wifi.p2p.WifiP2pSettings;
 import com.android.settings.wifi.savedaccesspoints2.SavedAccessPointsWifiSettings2;
 import com.android.settings.wifi.tether.WifiTetherSettings;
 
-import com.altho.setting.altho
-
 public class SettingsGateway {
 
     /**
@@ -325,9 +323,7 @@ public class SettingsGateway {
             NetworkProviderSettings.class.getName(),
             NetworkSelectSettings.class.getName(),
             AlarmsAndRemindersDetails.class.getName(),
-            MediaManagementAppsDetails.class.getName(),
-            MediaManagementAppsDetails.class.getName(),
-	    altho.class.getName()
+            MediaManagementAppsDetails.class.getName()
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
@@ -368,6 +364,5 @@ public class SettingsGateway {
             Settings.MyDeviceInfoActivity.class.getName(),
             Settings.ModuleLicensesActivity.class.getName(),
             UserBackupSettingsActivity.class.getName(),
-            Settings.althoActivity.class.getName(),
     };
 }
